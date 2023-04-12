ee1cf8f8a5b9828a8e98bb8f062c89ce20ca40e2
SimpKala <kalina.bujko@icloud.com>
2023-04-12 20:26:10 +0200

    Dodano skryptLab4.sh

--- skryptLab4.sh
+++ skryptLab4.sh
@@ -1,28 +1,65 @@
 #!/bin/bash
 
-function help {
+function help() {
     echo "Usage: $0 [OPTIONS]" #standardowa pierwsza linia skryptu z opcją help
+    echo ""
     echo "OPTIONS: "
-    echo "--date      to show today's date"
-    echo "--logs [n]  to create 100 or n log files and to write file name, script name and date to created files"
-    echo "--help      for help"
+    echo " -d, --date      to show today's date"
+    echo " -l, --logs [NUM]  to create 100 or n log files and to write file name, script name and date to created files"
+    echo " -h, --help      for help"
+    echo " -e, --error [NUM] to create error files, default is 100"
+    echo " --init to    to clone repository and set PATH"
+    echo ""
 }
 
-function logs {
-    if [[ -z $1 ]]; then
-           n=100
-    else
-           n=$1
-    fi
+function logs() {
+    num_files=${1:-100}
 
     for i in $(seq 1 $n); do
            echo "File: log$i.txt" > log$i.txt
            echo "Script: $0" >> log$i.txt
            echo "Date: $(date)" >> log$i.txt
     done
+
+    echo "$num_files log files created."
 }
 
-while [[ %# -gt 0 ]]; do
+function error() {
+    num_files=${1:-}
+
+    for i in $(seq 1 $num_files)
+    do
+        echo "File name: error$i.txt" > error$i.txt
+        echo "Created by: $0" >> error$i.txt
+        echo "Date: $(date)" >> error$i.txt
+    done
+
+    echo "$num_files error files created"
+}
+
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    help
+elif [[ "$1" == "--date" || "$1" == "-d" ]]; then
+    echo "Today's date: $(date)"
+elif [[ "$1" == "--logs" || "$1" == "-1" ]]; then
+  if [[ -z "$2" ]]; then
+    logs
+  else
+    logs "$2"
+  fi
+elif [[ "$1" == "error" || "$1" == "-e" ]]; then
+  if [[ -z "$2" ]]; then
+    error
+  else
+    error "$2"
+  fi
+elif [[ "$1" == "-innit" ]]; then
+    git clone git@github.com:SimpKala/Lab4.git
+    export PATH=$PATH:$(pwd)/Lab4/bash-scripts
+else
+    echo "Invalid option. Use --help or -h to display help menu."
+fi
+while [[ $# -gt 0 ]]; do
     case "$1" in
            --date)
                echo "Today's date: $(date +%Y-%m-%d)"
